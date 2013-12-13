06d9c68cbf81c22d8e45517fd18aa81f40eb74f1
manish <manish@manish-Vostro-1450.(none)>
2013-12-13 12:28:17 +0530

    2 prog

--- 1.py
+++ 1.py
@@ -1,1 +1,4 @@
+## to run this use =>  python 1.py 
 print "hello manish";
+#$ chmod +x 1.py     # This is to make file executable
+#$./1.py
---
+++ 2.py
@@ -0,0 +1,6 @@
+item = True;
+if item:
+ print 	"True"
+else:
+ print "False"
+
